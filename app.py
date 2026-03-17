32065e7c2b9186837c5c93fb660070c20b13a487
Juan Osorio <91640935+juanosorio99@users.noreply.github.com>
2026-03-17 11:11:44 -0500

    Add files via upload
    cambio de numeros

--- app.py
+++ app.py
@@ -56,11 +56,11 @@ st.title('Datos Operaciones')
 with st.container(border=True): 
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.metric('Mínimo', f'{minimo:.0f}', border=True)
+        st.metric('Mínimo', f'{minimo:.0f}', '400',  border=True)
     with col2:
-        st.metric('Media', f'{media:.0f}', border=True)
+        st.metric('Media', f'{media:.0f}', '-5', border=True)
     with col3:
-        st.metric('Máximo', f'{maximo:.0f}', border=True)
+        st.metric('Máximo', f'{maximo:.0f}','190', border=True)
 
 
 with st.container(border=True): 
